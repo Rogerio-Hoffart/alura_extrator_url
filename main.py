dad7ae80816b4b8b8a727f910bcbef4bee7bffab
Rogerio-Hoffart <rjhmribeiro@outlook.com>
2023-04-22 18:31:40 -0300

    Update

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 url = "https://rogerbank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 
 print(url)
+
+url_base = url[8:28]
+
+print(url_base)
+
+url_parametros = url[29:45]
+
+print(url_parametros)
