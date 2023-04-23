e32c6f5f6a564434df930a8029ee0832d7eb8eff
Rogerio-Hoffart <rjhmribeiro@outlook.com>
2023-04-23 17:25:08 -0300

    Update

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 
 url = "https://rogerbank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 
-print(url)
+# Separa a base e os parametros
 
-url_base = url[8:28]
-
-print(url_base)
-
-url_parametros = url[29:45]
-
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
+
+# Busca o valor de um parametro
+
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
